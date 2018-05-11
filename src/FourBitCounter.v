bfe2f82b54efe101342183830af3be7817c7c42b
Ashish Rana <ashishrana160796@gmail.com>
2018-05-11 12:04:18 +0530

    vcd dump and monitor statement added

--- src/FourBitCounter.v
+++ src/FourBitCounter.v
@@ -1,3 +1,4 @@
+// An active low reset signal is used for resetting the counter.
 module counter (input clk, input rstn, output reg[3:0] out);
   always @ (posedge clk) begin
     if (! rstn)
@@ -8,26 +9,31 @@ module counter (input clk, input rstn, output reg[3:0] out);
 endmodule
 
 module tb_counter;
-  reg clk;
-  reg rstn;
-  wire [3:0] out;
+reg clk;
+reg rstn;
+wire [3:0] out;
  
-  counter   c0 ( .clk (clk),
-                 .rstn (rstn),
-                 .out (out));
+counter c0(.clk(clk), .rstn (rstn), .out (out));
+
+// Generate a clock that should be driven to design
+// This clock will flip its value every 5ns -> time period = 10ns -> freq = 100 MHz
+always #5 clk = ~clk;
+
+initial
+begin
+$dumpfile("vcd/FourBitCounter.vcd");
+$dumpvars(0,tb_counter);
+
+$display("Clock \t Reset \t Output");
+$monitor("%b %b %b", clk, rstn, out);
+
+clk <= 0;
+rstn <= 0;
  
-  // Generate a clock that should be driven to design
-  // This clock will flip its value every 5ns -> time period = 10ns -> freq = 100 MHz
-  always #5 clk = ~clk;
- 
-  initial begin
-    clk <= 0;
-    rstn <= 0;
- 
-    #20   rstn <= 1;                   
-    #80   rstn <= 0;
-    #50   rstn <= 1;
- 
-    #20 $finish;
-  end
+#20   rstn <= 1;                   
+#80   rstn <= 0;
+#50   rstn <= 1;
+#20
+$finish;
+end
 endmodule
